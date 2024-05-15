0c49aaf955662621199f48d061e1453c4d0bceab
stjansen-heroes <stefan.jansen@heroes.nl>
2024-05-15 12:45:58 +0200

    update api messages

--- avatar/interactive/api/message/__init__.py
+++ avatar/interactive/api/message/__init__.py
@@ -6,6 +6,8 @@ from datetime import datetime, timedelta
 # import pyodbc
 
 import azure.functions as func
+import openai
+import re
 
 search_endpoint = os.getenv("AZURE_SEARCH_ENDPOINT")
 search_key = os.getenv("AZURE_SEARCH_API_KEY") 
@@ -34,56 +36,6 @@ blue, end_blue = '\033[36m', '\033[0m'
 place_orders = False
 
 functions = [
-    # {
-    #     "name": "get_bonus_points",
-    #     "description": "Check the amount of customer bonus / loyalty points",
-    #     "parameters": {
-    #         "type": "object",
-    #         "properties": {
-    #             "account_id": {
-    #                 "type": "number",
-    #                 "description": "Four digit account number (i.e., 1005, 2345, etc.)"
-    #             },
-    #         },
-    #         "required": ["account_id"],
-    #     }
-    # },
-    # {
-    #     "name": "get_order_details",
-    #     "description": "Check customer account for expected delivery date of existing orders based on the provided parameters",
-    #     "parameters": {
-    #         "type": "object",
-    #         "properties": {
-    #             "account_id": {
-    #                 "type": "number",
-    #                 "description": "Four digit account number (i.e., 1005, 2345, etc.)"
-    #             },
-    #         },
-    #         "required": ["account_id"],
-    #     }
-    # },
-    # {
-    #     "name": "order_product",
-    #     "description": "Order a product based on the provided parameters",
-    #     "parameters": {
-    #         "type": "object",
-    #         "properties": {
-    #             "account_id": {
-    #                 "type": "number",
-    #                 "description": "Four digit account number (i.e., 1005, 2345, etc.)"
-    #             },
-    #             "product_name": {
-    #                 "type": "string",
-    #                 "description": "Name of the product to order (i.e., Elysian Voyager, Terra Roamer, AceMaster 3000, Server & Style)"
-    #             },
-    #             "quantity": {
-    #                 "type": "number",
-    #                 "description": "Quantity of the product to order (i.e., 1, 2, etc.)"
-    #             }
-    #         },
-    #         "required": ["account_id", "product_name", "quantity"],
-    #     }
-    # },
     {
     "name": "get_product_information",
     "description": "Find information about a product based on a user question. Use only if the requested information if not already available in the conversation context.",
@@ -105,66 +57,20 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     messages = json.loads(req.get_body())
 
-    response = chat_complete(messages, functions= functions, function_call= "auto")
+    response = chat_complete_data(messages)
 
     products = []
     
     try:
-        response_message = response["choices"][0]["message"]
+        response_message = response.choices[0].message
     except:
         logging.info(response)
 
-    # if the model wants to call a function
-    if response_message.get("function_call"):
-        # Call the function. The JSON response may not always be valid so make sure to handle errors
-        function_name = response_message["function_call"]["name"]
+    response_message.content = re.sub(r'\s*\[doc\d+\]', '', response_message.content)
 
-        available_functions = {
-                "get_bonus_points": get_bonus_points,
-                "get_order_details": get_order_details,
-                "order_product": order_product,
-                "get_product_information": get_product_information,
-        }
-        function_to_call = available_functions[function_name] 
+    messages.append({'role' : response_message.role, 'content' : response_message.content})
 
-        function_args = json.loads(response_message["function_call"]["arguments"])
-        function_response = function_to_call(**function_args)
-        # print(function_name, function_args)
-
-        # Add the assistant response and function response to the messages
-        messages.append({
-            "role": response_message["role"],
-            "function_call": {
-                "name": function_name,
-                "arguments": response_message["function_call"]["arguments"],
-            },
-            "content": None
-        })
-
-        if function_to_call == get_product_information:
-            product_info = json.loads(function_response)
-            # show product information after search for a different product that the current one
-            # if product_info['product_image_file'] != current_product_image:
-                
-            products = [display_product_info(product_info)]
-            current_product_image = product_info['product_image_file']
-            
-            # return only product description to LLM to avoid chatting about prices and image files 
-            function_response = product_info['description']
-
-        messages.append({
-            "role": "function",
-            "name": function_name,
-            "content": function_response,
-        })
-     
-        response = chat_complete(messages, functions= functions, function_call= "none")
-        
-        response_message = response["choices"][0]["message"]
-
-    messages.append({'role' : response_message['role'], 'content' : response_message['content']})
-
-    logging.info(json.dumps(response_message))
+    logging.info(response_message.content)
 
     response_object = {
         "messages": messages,
@@ -176,129 +82,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         status_code=200
     )
 
-# def execute_sql_query(query, connection_string=database_connection_string, params=None):
-#     """Execute a SQL query and return the results."""
-#     results = []
-#     print('database_connection_string', database_connection_string)
-    
-#     # Establish the connection
-#     with pyodbc.connect(connection_string) as conn:
-#         cursor = conn.cursor()
-        
-#         if params:
-#             cursor.execute(query, params)
-#         else:
-#             cursor.execute(query)
-        
-#         # If the query is a SELECT statement, fetch results
-#         if query.strip().upper().startswith('SELECT'):
-#             results = cursor.fetchall()
-        
-#         conn.commit()
-
-#     return results
-
-# def get_bonus_points(account_id):
-#     """Retrieve bonus points and its cash value for a given account ID."""
-     
-#     # Define the SQL query to retrieve loyalty_points for the given account_id
-#     query = "SELECT loyalty_points FROM Customers WHERE account_id = ?"
-
-#     # Execute the query with account_id as a parameter
-#     results = execute_sql_query(query, params=(account_id,))
-
-#     # If results are empty, return an error message in JSON format
-#     if not results:
-#         return json.dumps({"error": "Account not found"})
-
-#     # Get the loyalty_points value
-#     loyalty_points = results[0][0]
-
-#     # Convert loyalty_points to cash_value
-#     cash_value = loyalty_points / 9.5
-
-
-
-#     # Create a JSON object with the required keys and values
-#     response_json = json.dumps({
-#         "available_bonus_points": loyalty_points,
-#         "cash_value": cash_value
-#     })
-
-#     return response_json
-
-
-# def get_order_details(account_id):
-     
-#     # Get orders and corresponding product names for the account_id
-#     query = '''
-#         SELECT o.order_id, p.name as product_name, o.days_to_delivery
-#         FROM Orders o
-#         JOIN Products p ON o.product_id = p.id
-#         WHERE o.account_id = ?
-#     '''
-#     orders = execute_sql_query(query, params=(account_id,))
-    
-#     # Get today's date and calculate the expected delivery date for each order
-#     today = datetime.today()
-    
-#     # Create a JSON object with the required details
-#     order_details = [
-#         {
-#             "product_name": order.product_name,
-#             "expected_delivery_date": (today + timedelta(days=order.days_to_delivery)).strftime('%Y-%m-%d')
-#         }
-#         for order in orders
-#     ]
-    
-#     # Return the JSON object
-#     return json.dumps(order_details)
-
-# def order_product(account_id, product_name, quantity=1):
-     
-#     # Step 1: Find the maximum existing order_id
-#     query = "SELECT MAX(order_id) FROM Orders"
-#     results = execute_sql_query(query)
-#     max_order_id = results[0][0] if results[0][0] is not None else 0
-
-#     # Step 2 & 3: Find product ID and check stock
-#     query = "SELECT id, name, stock FROM Products WHERE LOWER(name) LIKE LOWER(?)"
-#     params = (f'%{product_name}%',)
-#     results = execute_sql_query(query, params=params)
-    
-#     # Handling no match found
-#     if not results:
-#         return json.dumps({"info": "No matching product found"})
-    
-#     product_id, product_name_corrected, stock = results[0]
-    
-#     # Check if the stock is sufficient
-#     if stock < quantity:
-#         return json.dumps({"info": "Insufficient stock"})
-    
-#     # Step 4: Place the order
-#     # Deducting the ordered quantity from the stock
-#     query = "UPDATE Products SET stock = stock - ? WHERE id = ?"
-#     params = (quantity, product_id)
-#     if place_orders: execute_sql_query(query, params=params)
-
-#     # Adding the order details to the Orders table
-#     days_to_delivery = 5
-#     for i in range(quantity):
-#         max_order_id += 1
-#         query = "INSERT INTO Orders (order_id, product_id, days_to_delivery, account_id) VALUES (?, ?, ?, ?)"
-#         params = (max_order_id, product_id, days_to_delivery, account_id)
-#         if place_orders: execute_sql_query(query, params=params)
-    
-#     # Step 5: Calculate the expected delivery date and return the JSON object
-#     today = datetime.now()
-#     expected_delivery_date = today + timedelta(days=days_to_delivery)
-    
-#     return json.dumps({
-#         "info": "Order placed",
-#         "product_name": product_name_corrected,
-#         "expected_delivery_date": expected_delivery_date.strftime('%Y-%m-%d')
-#     })
 
 def display_product_info(product_info, display_size=40):
     """ Display product information """
@@ -384,23 +167,31 @@ def get_product_information(user_question, categories='*', top_k=1):
     }
     return json.dumps(response_data)
 
-def chat_complete(messages, functions, function_call='auto'):
-    """  Return assistant chat response based on user query. Assumes existing list of messages """
-    
-    url = f"{AOAI_endpoint}/openai/deployments/{chat_deployment}/chat/completions?api-version={AOAI_api_version}"
+def chat_complete_data(messages):
+    client = openai.AzureOpenAI(
+    azure_endpoint=AOAI_endpoint,
+    api_key=AOAI_key,
+    api_version="2024-02-01",
+    )
 
-    headers = {
-        "Content-Type": "application/json",
-        "api-key": AOAI_key
-    }
+    completion = client.chat.completions.create(
+        model=chat_deployment,
+        messages=messages,
+        extra_body={
+            "data_sources":[
+                {
+                    "type": "azure_search",
+                    "parameters": {
+                        "endpoint": search_endpoint,
+                        "index_name": search_index_name,
+                        "authentication": {
+                            "type": "api_key",
+                            "key": search_key,
+                        }
+                    }
+                }
+            ],
+        }
+    )
 
-    data = {
-        "messages": messages,
-        "functions": functions,
-        "function_call": function_call,
-        "temperature" : 0,
-    }
-
-    response = requests.post(url, headers=headers, data=json.dumps(data)).json()
-
-    return response
+    return completion
